7335544c29242a7d3db41eaf0b10bdace3ed1114
Ousmane <dioufism@gmail.com>
2021-05-04 12:38:43 -0400

    Initial Commit

---
+++ SwiftUIOnbordingFlow/SwiftUIOnbordingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOnbordingFlowApp.swift
+//  SwiftUIOnbordingFlow
+//
+//  Created by ousmane diouf on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOnbordingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
