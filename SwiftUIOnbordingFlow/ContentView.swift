2726430a822dd46471c9c672e900168a0ed9cfdb
Ousmane <dioufism@gmail.com>
2021-05-05 22:46:58 -0400

    created a reusable component that will used throughout the flow learned the use of the @state property which shows the view learned to present the View using Screen cover Vstacks are equivalent to stackViews body:  some View {} is a way to tell SwiftUI that we are trying to show some view(View, button...any type of component)

--- SwiftUIOnbordingFlow/ContentView.swift
+++ SwiftUIOnbordingFlow/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var shouldShowOnboarding: Bool =  true // determines whether we shiuld show the onboarding
+    
     var body: some View {
         NavigationView{
             VStack{
@@ -15,11 +18,59 @@ struct ContentView: View {
         }
             .navigationTitle("Home")
       }
-        
+        .fullScreenCover(isPresented: $shouldShowOnboarding) { // shows onboarding modally
+            OnboardingView()
+        }
+    }
+    
+}
+
+//create an onboarding view
+
+struct OnboardingView: View { // this is how we create a view suing swiftui
+    
+    var body: some View { // every single view needs a body
+        TabView{
+            
+            PageView()
+                .background(Color.red)
+            
+            PageView()
+                .background(Color.blue)
+            
+            PageView()
+                .background(Color.green)
+            
+            PageView()
+                .background(Color.orange)
+            
+        }
+        .tabViewStyle(PageTabViewStyle()) // adds swiping style
     }
 }
 
-//create a view
+// creating a reusable component
+struct PageView: View{
+    
+    var body: some View {
+        VStack{
+            Image(systemName: "bell")
+                .resizable()
+                .padding()
+            
+            Text("Push Notification")
+                .font(.system(size: 30))
+                .padding()
+            
+            Text("Enable Notification")
+                .font(.system(size: 30))
+                .foregroundColor(Color(.secondaryLabel))
+                .padding()
+
+        }
+        
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
